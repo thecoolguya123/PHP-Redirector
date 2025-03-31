489b705be85eeb10ed330ca10ddca3f6047f9647
thecoolguya123 <144173586+thecoolguya123@users.noreply.github.com>
2025-03-31 22:15:16 +0200

    Add files via upload

---
+++ project3.php
@@ -0,0 +1,24 @@
+<?php
+$url = isset($_GET['url']) ? $_GET['url'] : null; 
+if($url == null){
+   echo "<h1>400 Bad Request: URL was not specified.<br> Specify the URL by adding `?url=http://[url]/` as an extension on the current URL.</h1>";
+}
+else{
+    if(filter_var($url, FILTER_VALIDATE_URL)) {
+    $parsed_url = parse_url($url);
+    if (isset($parsed_url['scheme']) && in_array($parsed_url['scheme'], ['http', 'https'])) {
+    echo "<h3><code>Redirecting you to `$url`...</code></h3>";
+    ob_flush();
+    flush(); 
+    sleep(3); 
+    echo "<script>
+              window.location.href = '$url';
+        </script>";
+    } else{
+    echo "<h1>400 Bad Request: Only HTTP URLs allowed.</h1>";
+    }
+    } else{
+        echo "<h1>400 Bad Request: Invalid URL format<br>Add the `http://` header before `$url`.</h1>";
+    }
+}
+?>
