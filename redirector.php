5179d5b7a96cdb2c2dcd8acb7f094caaacd799e7
thecoolguya123 <144173586+thecoolguya123@users.noreply.github.com>
2025-04-01 10:37:03 +0200

    open source

--- redirector.php
+++ redirector.php
@@ -7,9 +7,9 @@ else{
     if(filter_var($url, FILTER_VALIDATE_URL)) { 
     $parsed_url = parse_url($url);
     if (isset($parsed_url['scheme']) && in_array($parsed_url['scheme'], ['http', 'https'])) {  // Checking if it is HTTP
-    echo "<h3><code>Redirecting you to `$url`...</code></h3>"; 
+    echo "<h3><code>Redirecting you to `$url` ...</code></h3>"; 
     ob_flush();
-    flush(); 
+    flush(); // Making sure redirection message gets sent to the browser
     sleep(3); 
     echo "<script>
        window.location.href = "$url";
